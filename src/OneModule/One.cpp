03c166d8bed25d1df64d4a5ae4f48ca0a8725e1d
liuke <m18611419854@163.com>
2025-10-06 23:29:55 +0800

    one module

---
+++ src/OneModule/One.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 刘科 on 2025/10/4.
+//
+
+#include <iostream>
+#include <OneModule/One.h>
+
+One::One() {
+    std::cout << "hello world one\n";
+}
+
---
+++ src/OneModule/include/OneModule/One.h
@@ -0,0 +1,17 @@
+//
+// Created by 刘科 on 2025/10/4.
+//
+
+#ifndef ONE_H
+#define ONE_H
+
+
+
+class One {
+public:
+    explicit One();
+};
+
+
+
+#endif //ONE_H
